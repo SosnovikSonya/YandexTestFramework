07b4f30406dadd281a7ff2b3e8138fab74e8c1ef
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2020-12-11 17:49:34 +0300

    Create market and music pages tests

---
+++ YandexTestFramework/YandexTestFramework/Tests/YandexMarketPageTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexTestFramework.Configuration;
+using YandexTestFramework.Pages;
+using YandexTestFramework.Pages.TabNavigationPages;
+using YandexTestFramework.Utils;
+
+namespace YandexTestFramework.Tests
+{
+    [TestFixture]
+    public class YandexMarketPageTests : BaseTest
+    {
+        private MainPage mainPage;
+        private YandexMarketPage yandexMarketPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mainPage = new MainPage(WebDriver);
+            yandexMarketPage = new YandexMarketPage(WebDriver);
+            mainPage.TabMarketClick();
+            windowActions.SwitchBetweenWindows(WebDriver, 1);
+        }
+
+        [TestCase(BrowserType.Chrome)]
+        public void YandexMarket_AddToComparison(BrowserType browser)
+        {
+            yandexMarketPage.EnterSearchText(TestData.searchInMarket);
+            yandexMarketPage.SearchButtonClick();
+
+            var addButtons = yandexMarketPage.GetAddToComparisonButtons();
+            yandexMarketPage.AddToComparison(addButtons[0]);
+            yandexMarketPage.AddToComparison(addButtons[1]);
+
+            yandexMarketPage.CompareButtonClick();
+
+            var items = yandexMarketPage.GetItemsInComparisonList();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(2, items.Count, "Assert there are two item in the list to compare");
+                Assert.IsTrue(items.ElementAt(0).Text.Contains(TestData.searchInMarket), "Assert first item's text contains search text");
+                Assert.IsTrue(items.ElementAt(1).Text.Contains(TestData.searchInMarket), "Assert second item's text contains search text");
+            });
+        }
+
+        [TestCase(BrowserType.Chrome)]
+        public void YandexMarket_DeleteFromComparison(BrowserType browser)
+        {
+            yandexMarketPage.EnterSearchText(TestData.searchInMarket);
+            yandexMarketPage.SearchButtonClick();
+
+            var addButtons = yandexMarketPage.GetAddToComparisonButtons();
+            yandexMarketPage.AddToComparison(addButtons[0]);
+            yandexMarketPage.AddToComparison(addButtons[1]);
+
+            yandexMarketPage.CompareButtonClick();
+
+            var items = yandexMarketPage.GetItemsInComparisonList();
+            Assert.AreEqual(2, items.Count, "Assert there are two item in the list to compare");
+
+            yandexMarketPage.DeleteFromComparison();
+
+            Assert.IsTrue(yandexMarketPage.GetNoItemsText().Displayed, "Assert text about empty comparison appeared");
+        }
+
+       
+    }
+}
---
+++ YandexTestFramework/YandexTestFramework/Tests/YandexMusicPageTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexTestFramework.Configuration;
+using YandexTestFramework.Pages;
+using YandexTestFramework.Pages.LoginPages;
+using YandexTestFramework.Pages.TabNavigationPages;
+using YandexTestFramework.Utils;
+
+namespace YandexTestFramework.Tests
+{
+    [TestFixture]
+    public class YandexMusicPageTests : BaseTest
+    {
+        private MainPage mainPage;
+        private YandexMusicPage yandexMusicPage;
+        private LoginPage loginPage;
+        private MailPage mailIncomingMessagesPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mainPage = new MainPage(WebDriver);
+            yandexMusicPage = new YandexMusicPage(WebDriver);
+            loginPage = new LoginPage(WebDriver);
+            mailIncomingMessagesPage = new MailPage(WebDriver);
+
+            mainPage.MailLoginClick();
+            windowActions.SwitchBetweenWindows(WebDriver, 1);
+            loginPage.SignIn(TestData.validLogin, TestData.validPassword);
+            mailIncomingMessagesPage.YandexHeaderClick();
+        }
+
+        [TestCase(BrowserType.Chrome)]
+        public void YandexMusic_SearchMusic(BrowserType browser)
+        {
+            mainPage.TabMusicClick();
+            windowActions.SwitchBetweenWindows(WebDriver, 2);
+            yandexMusicPage.AdvertisementCloseClick();
+            yandexMusicPage.EnterSearchInput(TestData.searchInMusic);
+            yandexMusicPage.SearchResultClick();
+            var artist = yandexMusicPage.GetArtistTitle();
+            Assert.AreEqual(TestData.artist, artist, "Actual and expected artists are not equal");
+
+            var popularAlbums = yandexMusicPage.GetPopularAlbumsArtist();
+            foreach (var album in popularAlbums)
+            {
+                Assert.AreEqual(TestData.artist, album.Text, "Actual and expected artists in popular albums are not equal");
+            }
+        }
+    }
+}
