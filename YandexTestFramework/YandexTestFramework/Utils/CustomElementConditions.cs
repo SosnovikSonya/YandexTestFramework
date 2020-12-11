2ffc915cb3bb5256a8c03c1e5ad37f54a2ea713a
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2020-12-11 16:21:35 +0300

    Some waiters added

---
+++ YandexTestFramework/YandexTestFramework/Utils/CustomElementConditions.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexTestFramework.Utils
+{
+    class CustomElementConditions
+    {
+        public bool IsElementPresentAndVisible(IWebElement webElement)
+        {
+            try
+            {
+                return webElement.Displayed;
+            }
+            catch (Exception ex)
+            {
+                if (ex is NoSuchElementException || ex is ElementNotVisibleException || ex is StaleElementReferenceException)
+                {
+                    return false;
+                }
+                if (ex.InnerException != null && ex.InnerException is StaleElementReferenceException)
+                {
+                    return false;
+                }
+                throw ex;
+            }
+        }
+
+        public bool IsElementEnabled(IWebElement webElement)
+        {
+            try
+            {
+                return webElement.Enabled;
+            }
+            catch (Exception ex)
+            {
+                if (ex is NoSuchElementException || ex is ElementNotVisibleException || ex is StaleElementReferenceException)
+                {
+                    return false;
+                }
+                if (ex.InnerException != null && ex.InnerException is StaleElementReferenceException)
+                {
+                    return false;
+                }
+                throw ex;
+            }
+        }
+    }
+}
---
+++ YandexTestFramework/YandexTestFramework/Utils/CustomWaiters.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexTestFramework.Configuration;
+
+namespace YandexTestFramework.Utils
+{
+    class CustomWaiters
+    {
+        private CustomElementConditions elementCondition;
+
+        /// <summary>
+        /// Ждать пока элемент станет видимым
+        /// Явное ожидание
+        /// </summary>
+        public void WaitUntilVisible(IWebElement element)
+        {
+            elementCondition = new CustomElementConditions();
+            try
+            {
+                new WebDriverWait(DriverProvider.Driver, TimeSpan.FromSeconds(30))
+                    .Until(d => elementCondition.IsElementPresentAndVisible(element));
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine($"Timed out waiting for the item to appear: '{element}'\n Exception: {ex}");
+                throw new TimeoutException($"Timed out waiting for the item to appear: {element}\n", ex);
+            }
+        }
+
+        /// <summary>
+        /// Ожидает когда элемент станет кликабельным и возвращаем элемент
+        /// </summary>
+        public IWebElement WaitUntilClickable(IWebElement element)
+        {
+            elementCondition = new CustomElementConditions();
+            try
+            {
+                return new WebDriverWait(DriverProvider.Driver, TimeSpan.FromSeconds(30))
+                    .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(element));
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine($"The element has timed out to be clickable: '{element}'\n Exception: {ex}");
+                throw new TimeoutException($"The element has timed out to be clickable: {element}\n", ex);
+            }
+        }
+
+        /// <summary>
+        /// Ждать пока все элементы станут видимыми
+        /// Явное ожидание
+        /// </summary>
+        public void WaitUntilAllElementsVisible(IList<IWebElement> elements)
+        {
+            var readOnlyList = new ReadOnlyCollection<IWebElement>(elements);
+            elementCondition = new CustomElementConditions();
+            try
+            {
+                new WebDriverWait(DriverProvider.Driver, TimeSpan.FromSeconds(30))
+                    .Until(d => SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(readOnlyList));
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine($"Timed out waiting for the item to appear \n Exception: {ex}");
+                throw new TimeoutException($"Timed out waiting for the item to appear \n", ex);
+            }
+        }
+    }
+}
