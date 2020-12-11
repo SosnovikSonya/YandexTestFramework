74a939c9ff921c4d40e26ac46a62a4bf9c4b8dac
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2020-12-11 17:47:16 +0300

    Create settings pages

---
+++ YandexTestFramework/YandexTestFramework/Pages/SettingsPages/GeoPositionSettingsPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexTestFramework.Utils;
+using YandexTestFramework.Utils.Actions;
+
+namespace YandexTestFramework.Pages.SettingsPages
+{
+    public class GeoPositionSettingsPage : PageObject
+    {
+        public GeoPositionSettingsPage(IWebDriver webDriver) : base(webDriver) { }
+        PageElementsActions pageElementsActions = new PageElementsActions();
+        MouseActions mouseActions = new MouseActions();
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='city__front-input']")]
+        private IWebElement City { get; set; }
+        [FindsBy(How = How.XPath, Using = "//*[contains(@class, 'b-autocomplete-item_type_geo')]")]
+        private IWebElement CityResult { get; set; }
+
+        public void EnterCity(string city)
+        {
+            pageElementsActions.InputDataInTheField(City, city);
+        }
+
+        public MainPage CityResultClick()
+        {
+            pageElementsActions.ClickTheButtonWhenIsVisible(CityResult);
+            return new MainPage(WebDriver);
+        }
+
+        public MainPage ChangeCity(string city)
+        {
+            pageElementsActions.InputDataInTheField(City, city);
+            mouseActions.MoveMouseTo(WebDriver, CityResult);
+            mouseActions.MouseClick(WebDriver, CityResult);
+            return new MainPage(WebDriver);
+        }
+    }
+}
---
+++ YandexTestFramework/YandexTestFramework/Pages/SettingsPages/LanguageSettingsPage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexTestFramework.Utils;
+using YandexTestFramework.Utils.Actions;
+
+namespace YandexTestFramework.Pages.SettingsPages
+{
+    public class LanguageSettingsPage : PageObject
+    {
+        public LanguageSettingsPage(IWebDriver webDriver) : base(webDriver) { }
+
+        PageElementsActions pageElementsActions = new PageElementsActions();
+
+        [FindsBy(How = How.XPath, Using = "//*[contains(@class, 'select__button')]")]
+        private IWebElement LanguageDropDownButton { get; set; }
+        [FindsBy(How = How.XPath, Using = "//span[text()='" + TestData.languageToChange + "']/..")]
+        private IWebElement LanguageToChange { get; set; }
+        [FindsBy(How = How.XPath, Using = "//*[contains(@class, 'form__save')]")]
+        private IWebElement SaveButton { get; set; }
+
+        public void LanguageDropDownButtonClick()
+        {
+            pageElementsActions.ClickTheButtonWhenIsClickable(LanguageDropDownButton);
+        }
+
+        public void LanguageToChangeClick()
+        {
+            pageElementsActions.ClickTheButtonWhenIsClickable(LanguageToChange);
+        }
+
+        public MainPage SaveButtonClick()
+        {
+            pageElementsActions.ClickTheButtonWhenIsClickable(SaveButton);
+            return new MainPage(WebDriver);
+        }
+    }
+}
