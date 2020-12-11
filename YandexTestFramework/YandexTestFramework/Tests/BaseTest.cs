d4f179bd5934c693d081d0cc33d088cdf6a89ace
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2020-12-11 17:48:49 +0300

    Create login page tests

---
+++ YandexTestFramework/YandexTestFramework/Tests/BaseTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexTestFramework.Configuration;
+using YandexTestFramework.Pages;
+using YandexTestFramework.Pages.LoginPages;
+using YandexTestFramework.Utils;
+using YandexTestFramework.Utils.Actions;
+
+namespace YandexTestFramework.Tests
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        protected IWebDriver WebDriver { get; private set; }
+        protected MainPage MainPage;
+        protected LoginPage LoginPage;
+        protected WindowActions windowActions;
+
+        [SetUp]
+        public void BaseTestSetUp()
+        {
+            BrowserType browser;
+            Enum.TryParse(TestContext.CurrentContext.Test.Arguments[0].ToString(), out browser);
+            DriverProvider.InitDriver(browser);
+            WebDriver = DriverProvider.Driver;
+            WebDriver.Navigate().GoToUrl(TestData.baseUrl);
+            windowActions = new WindowActions();
+        }
+
+        [TearDown]
+        public void BaseTearDown()
+        {
+            DriverProvider.CleanupDriver();
+        }
+    }
+}
---
+++ YandexTestFramework/YandexTestFramework/Tests/LoginPageTests.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexTestFramework.Configuration;
+using YandexTestFramework.Pages;
+using YandexTestFramework.Pages.LoginPages;
+using YandexTestFramework.Utils;
+
+namespace YandexTestFramework.Tests
+{
+    [TestFixture]
+    class LoginPageTests : BaseTest
+    {
+        private LoginPage loginPage;
+        private MainPage mainPage;
+        private MailPage mailIncomingMessagesPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mainPage = new MainPage(WebDriver);
+            loginPage = new LoginPage(WebDriver);
+            mailIncomingMessagesPage = new MailPage(WebDriver);
+        }
+
+        [TestCase(BrowserType.Chrome)]
+        public void YandexLogin_ValidCredentials(BrowserType browser)
+        {
+            mainPage.MailLoginClick();
+            windowActions.SwitchBetweenWindows(WebDriver, 1);
+            loginPage.EnterLogin(TestData.validLogin);
+            loginPage.ClickSignIn();
+            loginPage.EnterPassword(TestData.validPassword);
+            loginPage.ClickSignIn();
+
+            var userName = mailIncomingMessagesPage.GetUserName();
+
+            Assert.Multiple(() =>
+            {
+                Assert.NotNull(userName, "User name is null");
+                Assert.AreEqual(TestData.validLogin, userName, "User names are not equal");
+            });
+        }
+
+        [TestCase(BrowserType.Chrome)]
+        public void YandexLogout(BrowserType browser)
+        {
+            mainPage.MailLoginClick();
+            windowActions.SwitchBetweenWindows(WebDriver, 1);
+            loginPage.EnterLogin(TestData.validLogin);
+            loginPage.ClickSignIn();
+            loginPage.EnterPassword(TestData.validPassword);
+            loginPage.ClickSignIn();
+
+            mailIncomingMessagesPage.ClickOnUserName();
+
+            var loginUrl = windowActions.GetCurrentUrl(WebDriver);
+            mailIncomingMessagesPage.Logout();
+            var logoutUrl = windowActions.GetCurrentUrl(WebDriver);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreNotEqual(loginUrl, logoutUrl, "Url after logout didn't change");
+                Assert.IsTrue(loginPage.GetLoginButton().Displayed);
+            });
+
+        }
+
+        [TestCase(BrowserType.Chrome)]
+        public void YandexLogin_InvalidLogin(BrowserType browser)
+        {
+            mainPage.MailLoginClick();
+            windowActions.SwitchBetweenWindows(WebDriver, 1);
+            loginPage.EnterLogin(TestData.invalidLogin);
+            loginPage.ClickSignIn();
+            Assert.NotNull(loginPage.GetInvalidCredentialText(), "Error message is null");
+        }
+
+        [TestCase(BrowserType.Chrome)]
+        public void YandexLogin_InvalidPassword(BrowserType browser)
+        {
+            mainPage.MailLoginClick();
+            windowActions.SwitchBetweenWindows(WebDriver, 1);
+            loginPage.EnterLogin(TestData.validLogin);
+            loginPage.ClickSignIn();
+            loginPage.EnterPassword(TestData.invalidPassword);
+            loginPage.ClickSignIn();
+            Assert.NotNull(loginPage.GetInvalidCredentialText(), "Error message is null");
+        }
+
+    }
+}
