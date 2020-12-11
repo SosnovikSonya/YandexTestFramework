d4d746e1e5408ec41305e2534c2964b438e4888a
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2020-12-11 16:09:25 +0300

    Driver configuration

---
+++ YandexTestFramework/YandexTestFramework/Configuration/DriverProvider.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+
+namespace YandexTestFramework.Configuration
+{
+    public static class DriverProvider
+    {
+        public static void InitDriver(BrowserType browser)
+        {
+            GetDriverInstance(browser);
+        }
+
+        public static IWebDriver Driver;
+
+        public static void CleanupDriver()
+        {
+            Driver.Dispose();
+        }
+
+        private static IWebDriver GetDriverInstance(BrowserType browser)
+        {
+            switch (browser)
+            {
+                case BrowserType.Chrome:
+                    return GetChromeDriver();
+                case BrowserType.Firefox:
+                    return GetFirefoxDriver();
+                case BrowserType.IE:
+                    return GetIEDriver();
+                case BrowserType.Edge:
+                    return GetEdgeDriver();
+                default:
+                    throw new ArgumentException("Unrecognized driver type");
+            }
+        }
+
+        //TODO move to another class
+        private static void ConfigureDriver(IWebDriver driver)
+        {
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+        }
+
+        private static IWebDriver GetChromeDriver()
+        {
+            Driver = new ChromeDriver();
+            ConfigureDriver(Driver);
+            return Driver;
+        }
+
+        private static IWebDriver GetFirefoxDriver()
+        {
+            Driver = new FirefoxDriver();
+            ConfigureDriver(Driver);
+            return Driver;
+        }
+
+        private static IWebDriver GetIEDriver()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IWebDriver GetEdgeDriver()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
