e31e0778ca9366bc6996fa1cf1d6b31407026ce0
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2020-12-11 16:22:18 +0300

    Create test data

---
+++ YandexTestFramework/YandexTestFramework/Utils/TestData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexTestFramework.Utils
+{
+    class TestData
+    {
+        public const string baseUrl = "https://yandex.by/";
+        public const string validLogin = "AutotestUser";
+        public const string validPassword = "AutotestUser123";
+        public const string invalidLogin = "NoAutotestUser";
+        public const string invalidPassword = "NoAutotestUser123";
+        public const string languageToChange = "Українська";
+        public const string CurrentLanguageAbbreviation = "Ukr";
+        public const string City = "Лондон";
+        public const string CityToChange = "Париж";
+        public const string searchInMarket = "Note 8";
+        public const string categoryToSort = "Холодильники";
+        public const string widthInputTag = "50";
+        public const string searchInMusic = "Metal";
+        public const string artist = "Metallica";
+        public const string searchInMusicForPlaying = "beyo";
+        public const string artistForPlaying = "Beyoncé";
+    }
+}
